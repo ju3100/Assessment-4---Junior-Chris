0ae392a2c096ec23c6dbf67bbb7206489173fa91
ju3100 <junior.kavick@univ.edu.vu>
2026-05-20 15:41:27 +1100

    Update api.js

--- api.js
+++ api.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!this is my first render:congratulations');
 });
 
+app.get('/api', (req, res) => {
+  res.sen('Hello');
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
