3911fc5d0ff00772191b4b3633ae02b4df27b290
ju3100 <junior.kavick@univ.edu.vu>
2026-05-27 14:18:53 +1100

    Update api.js

--- api.js
+++ api.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const visiteur = process.env.visiteur || 'Anonyme';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!this is my first render:congratulations');
+ app.get('/', (req, res) => {
+res.send('Hello World!this is my first render:congratulations');
+});
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: "ok",
+    environment: process.env.APP_ENV || "local"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
